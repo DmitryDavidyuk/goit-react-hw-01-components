529ed5d32c432c815020d8438aa225974fa8337e
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-07-02 15:26:55 +0300

    task 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Profile from './components/profile';
+import user from './user.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
